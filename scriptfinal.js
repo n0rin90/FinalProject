3662e16cfb4fe01a31f2123fa06b0fe623284627
n0rin90 <ine.camilla@yahoo.com>
2018-11-29 10:57:33 +0100

    Add files via upload

---
+++ scriptfinal.js
@@ -0,0 +1,16 @@
+/*var slideIndex = 0;
+bildegalleri();
+
+function bildegalleri() {
+
+    var i;
+    var hentBilder = document.getElementsByClassName("bilde");
+    for (i = 0; i < hentBilder.length; i++) {
+      hentBilder[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > hentBilder.length) {slideIndex = 1}
+    hentBilder.item(slideIndex-1).style.display = "block";
+    setTimeout(bildegalleri, 3000); 
+}
+*/
